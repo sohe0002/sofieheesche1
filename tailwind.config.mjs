3ae2f10c6251570758c0dc61c3c5c0f42d4268ad
Sofie <sohe0002@stud.kea.dk>
2024-09-26 16:08:30 +0200

    tailwind og andet css

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,28 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+            colors: {
+                beige: "#FFFBF6",
+                grøn: "#5D624C",
+            },
+
+            fontFamily: {
+                prata: ['Prata', 'system-ui'],
+                openSans: ['Open Sans', 'system-ui'],
+            },
+
+            keyframes: {
+                slide: {
+                  '0%': { transform: 'translateX(0)' },
+                  '100%': { transform: 'translateX(-100%)' },
+                }
+              },
+              animation: {
+                slide: 'slide 10s linear infinite',
+              },
+
+        },
 	},
 	plugins: [
         require('@tailwindcss/typography'),
@@ -10,4 +31,6 @@ export default {
         require('@tailwindcss/aspect-ratio'),
         require('@tailwindcss/container-queries'),
     ],
+
+    
 }
